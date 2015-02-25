6b1a303978c30ca6c82820065c199638129e91a0
Nicholas Blair <npblair@wisc.edu>
2015-02-25 12:08:29 -0600

    Add a second route and corresponding view

--- my-app-seed-war/src/main/webapp/my-app.js
+++ my-app-seed-war/src/main/webapp/my-app.js
@@ -2,10 +2,11 @@
 
 (function() {
 	
-	app.config(['$routeProvider',
-	            function($routeProvider) {
+	app.config(['$routeProvider', function($routeProvider) {
 			$routeProvider.when('/default', {
 				templateUrl: 'my-app.html'
+			}).when('/view2', {
+				templateUrl: 'my-app-view-2.html'
 			}).otherwise({
 				redirectTo: '/default'
 			});
