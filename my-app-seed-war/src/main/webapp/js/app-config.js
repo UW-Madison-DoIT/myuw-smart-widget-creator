ca11f86c858430db4656b12a30ed5317db4cf33a
Tim Levett <levett@wisc.edu>
2015-03-08 09:45:05 -0500

    Add in app-config.js file so you can overwrite portal api calls

---
+++ my-app-seed-war/src/main/webapp/js/app-config.js
@@ -0,0 +1,8 @@
+
+(function() {
+angular.module('app-config', [])
+.constant('SERVICE_LOC', {'sessionInfo' : 'json/sessionsample.json',
+                             'sidebarInfo' : 'samples/sidebar.json'
+                            });
+
+})();
