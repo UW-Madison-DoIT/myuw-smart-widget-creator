8a37bce7f3ea1bb621fae140142a4dea09579a66
Tim Levett <levett@wisc.edu>
2016-01-12 11:20:49 -0600

    hack together script to build static until we fix bower install

--- build.sh
+++ build.sh
@@ -1,8 +1,38 @@
 
 ## Build static
+# cd node_modules/uw-frame
+# npm run build-static
+# cd ../../
+
+## Build static hack until we fix bower install on frame to run with root if needed
+
 cd node_modules/uw-frame
-npm run build-static
+npm install
+## Reset
+rm -rf target
+mkdir -p target
+
+## Resources
+cp -r ../uw-frame-components/* ./target/
+
+## Copy over superstatic configuration for dev superstatic instance
+cp superstatic.json ./target
+
+## Note index.html is copied in via grunt script
+
+## Get bower stuff
+
+pushd target
+../../node_modules/bower/bin/bower --config.interactive=false --allow-root install
+popd
+
+grunt
+
 cd ../../
+
+## Build less
+../node_modules/less/bin/lessc -x target/css/themes/uw-madison.less > target/css/themes/uw-madison.css
+../node_modules/less/bin/lessc -x target/css/themes/uw-system.less > target/css/themes/uw-system.css
 
 ## Copy static to target
 rm -rf dist
